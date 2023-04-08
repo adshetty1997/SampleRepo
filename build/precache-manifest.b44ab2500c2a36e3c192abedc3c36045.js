e3b3d4f07cb4a5c6d15bed40c814b20ac47bc127
Adit Shetty <adshetty1997@gmail.com>
2023-04-08 23:58:55 +0530

    build folder

---
+++ build/precache-manifest.b44ab2500c2a36e3c192abedc3c36045.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294eff8195e2ce87fa2450bf79e1d466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "699f34c97a8983eefe74",
+    "url": "/static/js/2.1644abf7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1644abf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649cbdd142defbe3828e",
+    "url": "/static/js/main.92193625.chunk.js"
+  },
+  {
+    "revision": "6a1b03e774db89c8f7c5",
+    "url": "/static/js/runtime-main.31370bb3.js"
+  }
+]);
